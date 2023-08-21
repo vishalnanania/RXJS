0f7328bcd54135c83263109f511637b686a9597a
vishalnanania <vishal.nanania@gmail.com>
2023-08-21 14:14:58 -0400

    observable, from, of, interval

--- src/testrx.js
+++ src/testrx.js
@@ -1,9 +1,45 @@
-import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { of, Observable, interval, from } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
-of(1,2,3)
+// Observable
+const obj = new Observable((observer) => {
+    observer.next('hello');
+    observer.error('error');
+});
+
+obj.subscribe(
+    (value) => {
+        // console.log(value);
+    },
+    (error) => {
+        // console.log(error);
+    }
+);
+
+// operators ceration 
+
+//of
+of(1, 2, 3)
     .pipe(
-        map(x=>x*2)
-    ).subscribe((x)=>{
-        console.log(x);
+        map(x => x * 2)
+    ).subscribe((x) => {
+        // console.log(x);
     });
+
+//interval
+interval(1000).pipe(
+    take(2),
+    map(x => x ** 2)
+).subscribe((x) => {
+    // console.log(x)
+})
+
+//from
+
+from([10, 20, 30, 40, 50])
+    .pipe(
+        map(x => x * 2)
+    )
+    .subscribe((x) => {
+        console.log(x);
+    })
