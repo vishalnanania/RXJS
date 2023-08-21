770b28a807918364819455956d02225dd149ea53
vishalnanania <vishal.nanania@gmail.com>
2023-08-21 12:18:37 -0400

    added esm to run rxjs

---
+++ README.md
@@ -0,0 +1,9 @@
+RXJS
+
+npm i 
+run code with esm: node -r esm testrx.js
+
+
+steps to login vscode with github acc
+rm -rf ~/.git-credentials
+then login with github id and pass
---
+++ src/testrx.js
@@ -0,0 +1,9 @@
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+of(1,2,3)
+    .pipe(
+        map(x=>x*2)
+    ).subscribe((x)=>{
+        console.log(x);
+    });
